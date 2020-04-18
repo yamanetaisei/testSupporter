5640bf34b016a7d03cf7ecfc391bf29468b87e65
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-04-19 00:50:38 +0900

    開発開始

---
+++ testSupporter/testSupporter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testSupporter
+//
+//  Created by 山根大生 on 2020/04/19.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
