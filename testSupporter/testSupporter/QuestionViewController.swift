16e14be7cee176d88a91a31aed1e9486e9384e4b
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-04-27 23:35:21 +0900

    回答欄追加機能実装

--- testSupporter/testSupporter/QuestionViewController.swift
+++ testSupporter/testSupporter/QuestionViewController.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import UIKit
+import Eureka
+
+class QuestionViewController:FormViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        form +++
+            MultivaluedSection(multivaluedOptions: [.Reorder, .Insert, ],
+                               header: "Multivalued TextField",
+                               footer: ".Insert adds a 'Add Item' (Add New Tag) button row as last cell.") {
+                //公式ドキュメントのコピペ
+                $0.addButtonProvider = { section in
+                    return ButtonRow(){
+                        $0.title = "Add New Tag"
+                    }
+                }
+                                
+                $0.multivaluedRowToInsertAt = { index in
+                    return NameRow() {
+                        $0.placeholder = "Tag Name"
+                        print(index)
+                    }
+                }
+                $0 <<< NameRow() {
+                    $0.placeholder = "Tag Name"
+                }
+            }
+        }
+    }
+
+
