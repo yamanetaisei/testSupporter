14c3c67d2abdbd81e3ca4773822cf84fec1d2e2f
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-05-02 14:22:40 +0900

    リザルトがめん追加

---
+++ testSupporter/testSupporter/ResultViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ResultViewController.swift
+//  testSupporter
+//
+//  Created by 山根大生 on 2020/05/02.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultViewController: UIViewController{
+    
+}
