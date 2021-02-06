4ca14a47a1789ab606dc4a4b264d577a93c3f781
yamanetaisei <swiftmantaisei@gmail.com>
2021-02-06 17:22:44 +0900

    ファイルを追加

---
+++ testSupporter/testSupporter/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  testSupporter
+//
+//  Created by 山根大生 on 2021/01/26.
+//  Copyright © 2021 taisei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
